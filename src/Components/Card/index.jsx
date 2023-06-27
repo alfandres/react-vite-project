fe39969e706992a8d29369faff5266c7e4d09b3f
Andres Parra <alfandres2108@gmail.com>
2023-06-27 10:41:35 -0500

    se agrego una api para consumirla y poder pintar las cards

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,20 +1,20 @@
 
 
-function Card () {
+function Card ({data}) {
 
     return (
 
         <div className='bg-white cursor-pointer w-60 h-80 rounded-lg'> 
             <figure className='relative mb-2 w-full h-4/5'>
-                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-md m-2 px-3 py-0.5'>Men</span>
-                <img className='w-full h-full object-cover rounded-lg' src='https://img.freepik.com/foto-gratis/concepto-maqueta-camisa-ropa-sencilla_23-2149448792.jpg?w=740&t=st=1687817003~exp=1687817603~hmac=f9afd596214367e539d36b5000be2cf3cbc6ef32b61822bf00b00b651bd8e105' alt='T-Shirt' />
+                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-md m-2 px-3 py-0.5'>{data.category}</span>
+                <img className='w-full h-full object-cover rounded-lg' src={data.image} alt={data.title} />
                 <div className='absolute top-0  right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'>
                     +
                 </div>
             </figure>
             <p className='flex justify-between'>  
-                <span className='text-md font-medium'>T-Shirt</span>
-                <span className='text-lg font-bold'>35.000,00 COL$</span>
+                <span className='text-md font-medium'>{data.title}</span>
+                <span className='text-lg font-bold'>${data.price}</span>
             </p>
         </div>
 
