b3009164827d3eeedd9eaf22b53827f61d1b29bc
Andres Parra <alfandres2108@gmail.com>
2023-07-05 17:45:03 -0500

    se implemento la base para el apartado de myOrders

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,7 +1,9 @@
 import { useContext } from 'react';
 import { shopiContext } from '../../Context';
+import { Link } from 'react-router-dom';
 import Layout from '../../Components/Layout';
 import OrderCard from '../../Components/OrderCard';
+import { ChevronDoubleLeftIcon } from '@heroicons/react/24/solid';
 
 function MyOrder() {
 
@@ -10,7 +12,13 @@ function MyOrder() {
  
     return (
       <Layout>
-        MyOrder
+         <div className='flex items-center justify-center relative w-80 mb-6'>
+            <Link to='/MyOrders' className='absolute left-0'>
+              <ChevronDoubleLeftIcon className='h-6 w-6 text-black cursor-pointer'/>
+            </Link>
+            <h1>MyOrder</h1>
+          </div>
+
         <div className='flex flex-col w-80'>
           {
             context.order?.slice(-1)[0].products.map( product => (
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,11 +1,34 @@
-import Layout from "../../Components/Layout";
+import { useContext } from 'react';
+import { shopiContext } from '../../Context';
+import { Link } from 'react-router-dom';
+import Layout from '../../Components/Layout';
+import OrdersCard from '../../Components/OrdersCard';
+
+
 
 function MyOrders() {
+  const context = useContext(shopiContext);
 
     return (
       
         <Layout>
-          MyOrders
+          <div className='flex items-center justify-center relative w-80'>
+            <h1>MyOrders</h1>
+          </div>
+          
+          {
+            context.order.map( (order, index) => {
+              <Link 
+              key={index} 
+              to={`/MyOrders/${order.id}`}>
+                <OrdersCard 
+                  totalPrice={order.totalPrice} 
+                  totalProducts={order.totalProducts}  
+                />
+              </Link>
+            })
+          }
+          
         </Layout>
       
     );
